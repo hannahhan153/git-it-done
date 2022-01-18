93bd41f8dea1247bf5573ff1dac4f7219edf4deb
hannahhan153 <hannah.han@biola.edu>
2022-01-17 21:03:18 -0800

    update request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,13 @@
-var getUserRepos = function() {
-    var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+var getUserRepos = function(user) {
+    //format the github api url
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+
+    //make request to the url
+    fetch(apiURL).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
-        console.log("inside", response);
     });
-    console.log("outside");
-};
-
-getUserRepos();
+    };
+    
+    getUserRepos("spotify")
